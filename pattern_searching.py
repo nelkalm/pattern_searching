d28e76b5cd268cd8c5d521bbd795517a17acf280
Nelson Lu <nelkalmlu@gmail.com>
2022-06-03 18:53:53 -0700

    Final commit

--- pattern_searching.py
+++ pattern_searching.py
@@ -4,12 +4,17 @@ def pattern_search(text, pattern):
     print(pattern)
     for index in range(len(text)):
         print(f'Text Index: {index}')
+        match_count = 0
         for char in range(len(pattern)):
             print(f'Pattern Index: {char}')
             if pattern[char] == text[char + index]:
                 print('Matching index found')
+                print(f'Match Count: {match_count}')
+                match_count += 1
             else:
                 break
+        if match_count == len(pattern):
+            print(f'{pattern} found at index {index}')
 
 
 text = "HAYHAYNEEDLEHAYHAYHAYNEEDLEHAYHAYHAYHAYNEEDLE"
